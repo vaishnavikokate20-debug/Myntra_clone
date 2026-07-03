f6e7e2cfe2f562adc95cb048e6cee6e65042ae9d
vaishnaviKokate20 <vaishnavikokate20@gmail.com>
2026-07-03 15:21:52 +0530

    myntra project added

---
+++ Script.js
@@ -0,0 +1,39 @@
+
+
+const products = [
+ {
+   category: "men",
+   name: "T-Shirt",
+   price: 999,
+   rating: 4.5,
+   image: "products-images/Tshirt.png"
+ },
+ {
+   category: "men",
+   name: "T-Shirt",
+   price: 1299,
+   rating: 4.2,
+   image: "products-images/2.png"
+ },
+ {
+   category: "men",
+   name: "T-Shirt & Lower",
+   price: 1299,
+   rating: 4.2,
+   image: "products-images/3.png"
+ }
+ 
+];
+
+const container = document.getElementById("product-container");
+
+products.forEach((product)=>{
+   container.innerHTML += `
+      <div>
+         <img src="${product.image}" width="200">
+         <h3>${product.name}</h3>
+         <p>⭐ ${product.rating}</p>
+         <p>₹${product.price}</p>
+      </div>
+   `;
+});
